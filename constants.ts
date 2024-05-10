13a9d0d6f5a84db70c222a3852155997723b1f39
Paradox <will@paranodes.io>
2024-05-10 02:02:03 +0100

    Temp constants

--- constants.ts
+++ constants.ts
@@ -1,12 +1,10 @@
-export const KSM_ASSETHUB_WSS = `ws://192.168.250.177:9944`;
-export const KSM_BRIDGEHUB_WSS = `ws://192.168.250.178:9944`;
-export const KSM_CORETIME_WSS = `ws://192.168.250.211:9944`;
-
-
+export const KSM_ASSETHUB_WSS = `wss://sys.dotters.network/statemint`;
+export const KSM_BRIDGEHUB_WSS = `wss://sys.dotters.network/bridgehub-polkadot`;
+export const KSM_CORETIME_WSS = `wss://sys.dotters.network/collectives-polkadot`;
 
 export const DOT_ASSETHUB_WSS = `wss://sys.dotters.network/statemine`;
 export const DOT_BRIDGEHUB_WSS = `wss://sys.dotters.network/bridgehub-kusama`;
-export const DOT_COLLECTIVES_WSS = `wss://kusama-coretime-rpc.polkadot.io`;
+export const DOT_COLLECTIVES_WSS = `wss://sys.dotters.network/coretime-kusama`;
 
 //export const DOT_ASSETHUB_WSS = `ws://192.168.250.179:9944`;
 //export const DOT_BRIDGEHUB_WSS = `ws://192.168.250.180:9944`;
@@ -15,6 +13,8 @@ export const DOT_COLLECTIVES_WSS = `wss://kusama-coretime-rpc.polkadot.io`;
 export const PARACHAIN_BLOCK_TIME = 12;
 
 //export const KSM_WSS = `ws://192.168.250.175:9944`;
+//export const DOTWSS = `ws://192.168.250.176:9944`;
+
 export const KSM_WSS = `wss://rpc-kusama.luckyfriday.io`;
 export const DOT_WSS = `wss://rpc-polkadot.luckyfriday.io`;
 
